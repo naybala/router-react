586faab5e8ba38ff35b7da78b9c9b1077196eed0
naybala <naybalaismylife.nbl@gmail.com>
2022-07-20 23:39:05 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from "react";
+
+
+import "./App.css"
+function App() {
+  return (
+    <Fragment>
+      <h1 className="text-primary">Hello This is Router Test</h1>
+    </Fragment>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from"react";
+import {createRoot} from"react-dom/client";
+import App from"./App";
+createRoot(document.getElementById("root")).render(<App/>);
+
+// const root= document.getElementById("root");
+// ReactDom.render(<App/>,root);
+
+            
