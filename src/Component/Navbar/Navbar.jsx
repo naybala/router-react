6ec6d06fd6252c4bfe669800c3c13c630065586b
naybala <naybalaismylife.nbl@gmail.com>
2022-07-21 12:56:01 +0630

    nav active done

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 const imgurl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAwp8sJeH4aRAmPH0_Zf6ZK-Z1H2fuigeW3A&usqp=CAU";
 
-
-
 const Navbar = () => {
     const clickNav = () => {
         const removeNav = document.querySelector('.collapse');
         removeNav.classList.remove("show");
     }
+    const navActive = (e) => {
+        const nav = document.querySelectorAll(".nav-link");
+        nav.forEach(function (btn) {
+            btn.classList.remove("active");
+        });
+        e.target.classList.add("active");
 
-
-
+    }
     return (
         <>
             <nav className="navbar navbar-expand-lg py-3 fixed-top bg-light" >
@@ -31,28 +34,25 @@ const Navbar = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navmenu">
+                    <div className="collapse navbar-collapse" id="navmenu" onClick={navActive}>
                         <ul className="navbar-nav ms-auto" onClick={clickNav}>
-
                             <li className="nav-item">
-                                <Link to="/" className="nav-link">Home</Link>
+                                <Link to="/" className="nav-link active" data-id="home">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/about" className="nav-link">About</Link>
+                                <Link to="/about" className="nav-link" data-id="about">About</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/blog" className="nav-link">Blog</Link>
+                                <Link to="/blog" className="nav-link" data-id="blog">Blog</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/contact" className="nav-link">Contact Us</Link>
+                                <Link to="/contact" className="nav-link" data-id="contact">Contact Us</Link>
                             </li>
-                            {/* <a href="!#" className="nav-link">Contact Us</a> */}
                         </ul>
                     </div>
                 </div>
             </nav>
-            <br /><br /><br />
-            <br /><br /><br /> <br />
+            <br /><br /><br /><br />
         </>
     )
 }
